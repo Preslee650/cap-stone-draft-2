4aa1c171766b701f0ed07bc95a9161f1d7ceb758
Michael Matinez <116032312+Preslee650@users.noreply.github.com>
2022-12-02 16:42:30 -0800

    location and parks type dropdown
    dropdown displays names of locations and park types.

--- scripts/parksDropDown.js
+++ scripts/parksDropDown.js
@@ -1,15 +1,16 @@
-window.onload = function locationDisplayDropDown() {
+window.onload = function () {
+  // load the dropdown list
+  locationDisplayDropDown();
+  parksTypeDropDown();
+};
+function locationDisplayDropDown() {
   // load the dropdown list
   const dropDownList = document.getElementById("locationList");
   let length = locationsArray.length;
   for (let i = 0; i < length; i++) {
 
     // create the option element
-    let theOption = document.createElement("option");
-
-    // set the text and value of the option you created
-    theOption.textContent = locationsArray[i];
-    theOption.value = locationsArray[i];
+    let theOption = new Option(locationsArray[i], locationsArray[i]);
 
     // append the option as a child of (inside) the
     // select element
@@ -17,21 +18,25 @@ window.onload = function locationDisplayDropDown() {
   }
 };
 
-window.onload = function locationDisplayDropDown() {
+function parksTypeDropDown() {
   // load the dropdown list
-  const dropDownList = document.getElementById("locationList");
-  let length = locationsArray.length;
+  const dropDownList2 = document.getElementById("parksTypeList");
+  let length = parkTypesArray.length;
   for (let i = 0; i < length; i++) {
 
     // create the option element
-    let theOption = document.createElement("option");
-
-    // set the text and value of the option you created
-    theOption.textContent = locationsArray[i];
-    theOption.value = locationsArray[i];
+    let theOption2 = new Option(parkTypesArray[i], parkTypesArray[i]);
 
     // append the option as a child of (inside) the
     // select element
-    dropDownList.appendChild(theOption);
+    dropDownList2.appendChild(theOption2);
   }
 };
+
+/* const statesList = document.getElementById("statesList");
+let length = states.length;
+for (let i = 0; i < length; i++) {
+// create the option element and set the text and // value at the same time
+let theOption = new Option(states[i], abbrev[i]);
+// append the option as a child of (inside) the // select element statesList.appendChild(theOption);
+} */
