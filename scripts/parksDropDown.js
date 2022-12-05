27b4ad7af4ec4c7e6dd4f754e7ffc07aff95f9c0
Michael Matinez <116032312+Preslee650@users.noreply.github.com>
2022-12-05 15:00:05 -0800

    functioning dropdown buttons
    dropdown button works with selectedindex method.

--- scripts/parksDropDown.js
+++ scripts/parksDropDown.js
@@ -2,6 +2,7 @@ window.onload = function () {
   // load the dropdown list
   locationDisplayDropDown();
   parksTypeDropDown();
+  nationalParksDropDown();
 };
 function locationDisplayDropDown() {
   // load the dropdown list
@@ -15,6 +16,13 @@ function locationDisplayDropDown() {
     // append the option as a child of (inside) the
     // select element
     dropDownList.appendChild(theOption);
+  }
+  // looks up items in dropdowns options collection
+  if (locationList.selectedIndex >= 0) {
+    let text = locationList.options[locationList.selectedIndex].text;
+    let value = locationList.value;
+    /*  // test
+    alert(`Selected: ${text} \nValue: ${value}`);   */
   }
 };
 
@@ -31,12 +39,30 @@ function parksTypeDropDown() {
     // select element
     dropDownList2.appendChild(theOption2);
   }
+  if (parksTypeList.selectedIndex >= 0) {
+    let text = parksTypeList.options[parksTypeList.selectedIndex].text;
+    let value = parksTypeList.value;
+  }
 };
 
-/* const statesList = document.getElementById("statesList");
-let length = states.length;
-for (let i = 0; i < length; i++) {
-// create the option element and set the text and // value at the same time
-let theOption = new Option(states[i], abbrev[i]);
-// append the option as a child of (inside) the // select element statesList.appendChild(theOption);
-} */
+function nationalParksDropDown() {
+  // load the dropdown list
+  const dropDownList3 = document.getElementById("nationalParksList");
+  let length = nationalParksArray.length;
+  for (let i = 0; i < length; i++) {
+
+    // create the option element
+    let theOption3 = new Option(nationalParksArray[i].LocationName, nationalParksArray[i].LocationName);
+
+    // append the option as a child of (inside) the
+    // select element
+    dropDownList3.appendChild(theOption3);
+  }
+  if (locationList.selectedIndex >= 0) {
+    let text = nationalParksList.options[nationalParksList.selectedIndex].text;
+    let value = nationalParksList.value;
+  }
+};
+
+
+/* reminders: use alert("Selected: " + text + "\nValue: " + value); to test javascript. break every thing down in pseudo code */
