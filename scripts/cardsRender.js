0d4444e359dcd2246878dfff02f8f070bc46b0df
Michael Matinez <116032312+Preslee650@users.noreply.github.com>
2022-12-08 15:57:44 -0800

    presentation ready
    added styling and new homepage. linked pages together.

--- scripts/cardsRender.js
+++ scripts/cardsRender.js
@@ -1,22 +1,25 @@
-//need a list of filtered parks. empty cards container. then we add new cards based on parks parameter.
+// EMPTIES CARD CONTAINER THEN WE ADD NEW CARDS BASED ON PARKS PARAMETER
 function renderParksCards(parks) {
     let cardsMain = document.getElementById('cardsContainer')
-    //emptying cards container
+
+    // EMPTYING CARDS CONTAINER
     cardsMain.replaceChildren()
-    //wrapping cardTemplate in a loop.
+
+    // WRAPPING CARD TEMPLATE IN A LOOP
     let length = parks.length;
     for (let i = 0; i < length; i++) {
-    let park = parks[i]
+        let park = parks[i]
 
-        const cardTemplate = `<div class="card" style="width: 18rem;">
+        const cardTemplate = `<div class="card">
             <img class="card-img" src="/images/Bond-StoryImage_3.jpg">
             <div class="card-body">
                 <h5 class="card-title">${park.LocationName}</h5>
                 <p class="card-descrip">${park.Address}</p>
             </div>
         </div>`
+
         //innerHTML
         cardsMain.insertAdjacentHTML("beforeend", cardTemplate)
     }
-    
+
 }
--- scripts/parksDropDown.js
+++ scripts/parksDropDown.js
@@ -1,62 +1,62 @@
 window.onload = function () {
-  // load the dropdown list
+  // CALLING MY DROP-DOWN FUNCTIONS WHEN PAGE LOADS UP
   locationDisplayDropDown();
   parksTypeDropDown();
   nationalParksDropDown();
 };
 
+// DISPLAYING LOCATION DATA ARRAY IN DROP-DOWN
 function locationDisplayDropDown() {
-  // load the dropdown list
   const dropDownList = document.getElementById("locationList");
   let length = locationsArray.length;
   for (let i = 0; i < length; i++) {
 
-    // create the option element
+    // CREATING OPTION ELEMENT
     let theOption = new Option(locationsArray[i], locationsArray[i]);
 
-    // append the option as a child of (inside) the
-    // select element
+    // APPEND THE OPTION AS A CHILD OF (INSIDE) THE SELECT ELEMENT. BASICALLY STORING OPTIONS INSIDE DROP-DOWN
     dropDownList.appendChild(theOption);
   }
 }
 
+// DISPLAYING PARK TYPE DATA ARRAY IN DROP-DOWN
 function parksTypeDropDown() {
-  // load the dropdown list
   const dropDownList2 = document.getElementById("parksTypeList");
   let length = parkTypesArray.length;
   for (let i = 0; i < length; i++) {
 
-    // create the option element
     let theOption2 = new Option(parkTypesArray[i], parkTypesArray[i]);
 
-    // append the option as a child of (inside) the
-    // select element
     dropDownList2.appendChild(theOption2);
   }
+
+  // DETERMINES INDEX NUMBER OF SELECTED OPTION. BASICALLY DETERMINES AND LOOKS UP THE SELECTED OPTION
   if (parksTypeList.selectedIndex >= 0) {
     let text = parksTypeList.options[parksTypeList.selectedIndex].text;
     let value = parksTypeList.value;
   }
 };
 
+// DISPLAYING NATIONAL PARKS DATA ARRAY IN DROP-DOWN
 function nationalParksDropDown() {
   if (locationList.selectedIndex >= 0) {
     let text = locationList.options[locationList.selectedIndex].text;
     selectedLocationValue = locationList.value;
   }
+
+  // FILTERING DROP-DOWN RESULTS
   let parksByLocation = nationalParksArray.filter(park => park.State == selectedLocationValue);
-  // load the dropdown list
+
   const parksDropDown = document.getElementById("nationalParksList");
   parksDropDown.innerHTML = ""
   let length = parksByLocation.length;
   for (let i = 0; i < length; i++) {
 
-    // create the option element
+    // CREATING OPTIONS BY ONLY GETTING LOCATION NAME INFO FROM THE ARRAY
     let theOption3 = new Option(parksByLocation[i].LocationName, parksByLocation[i].LocationName);
 
-    // append the option as a child of (inside) the
-    // select element
     parksDropDown.appendChild(theOption3);
   }
+  // CALLING FUNCTION TO RENDER FILTERED DISPLAY CARDS
   renderParksCards(parksByLocation);
 };
